f494e900a4325af90f5d0bd5c8c6cd0473756eaf
Masud <masudrahman.dev@gmail.com>
2024-03-10 14:32:17 +0600

    font size and color config done

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <button className="rounded  bg-blue-500  px-4 py-2 text-base text-white">
-        sdf
+    <div className="max-[1440px] mx-auto">
+      <button className="text-title  rounded  bg-blue-500 px-4 py-2 ">
+        Hello There
       </button>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,72 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"Manrope", sans-serif', ...defaultTheme.fontFamily.sans],
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      primary: "#31B099",
+      midnight: "#1C2634",
+      dark: "#1A1C1E",
+      gray: "#6C7278",
+    },
+    extend: {
+      fontSize: {
+        // Semantic Naming
+        small: [
+          "0.75rem",
+          {
+            lineHeight: "1.2rem",
+            fontWeight: "500",
+          },
+        ],
+        // medium: "1rem",
+        // large: "1.25rem",
+        // heading: "2rem",
+        // subheading: "1.5rem",
+        // menu: "0.875rem",
+        // button: "1rem",
+        // link: "1rem",
+        // meta: "0.875rem",
+
+        // Functional Naming
+        body: "1rem",
+        caption: "0.875rem",
+        title: [
+          "1.125rem",
+          {
+            lineHeight: "1.6875rem",
+            fontWeight: "600",
+          },
+        ],
+        subtitle: [
+          "1.125rem",
+          {
+            lineHeight: "1.1.6875rem",
+            fontWeight: "500",
+          },
+        ],
+        label: [
+          "0.875rem",
+          {
+            lineHeight: "1.1.6875rem",
+            fontWeight: "500",
+          },
+        ],
+        // badge: "0.75rem",
+        // tag: "0.875rem",
+        // error: "0.875rem",
+        // help: "0.875rem",
+        // quote: "1.25rem",
+      },
+    },
   },
   plugins: [],
 };
